d92caab8041946cb6d9f2138e46b69c3e29da97f
Berkan KIRAN <berkankiran@hotmail.com>
2016-05-23 20:18:09 +0300

    Add files via upload

--- select_not_selection_lines.lua
+++ select_not_selection_lines.lua
@@ -1,17 +1,25 @@
-	script_name = "Select not Selection Lines"
+﻿	script_name = "Select not Selection Lines"
 	script_desription = "Seçmediğiniz satırları seçer."
 	script_version = "1"
 	script_author = "Magnum357"
 
 	function add_macro(subs,sel)
 	local idx, index = 0, {}
-	for si, li in ipairs(sel) do idx = idx + 1 index[idx] = li end
+	for si, li in ipairs(sel) do
+	idx = idx + 1
+	index[idx] = li
+	end
 	if index[1] ~= nil then
-	local k, vls, values = 1, 0, {}
+	local vls, values = 0, {}
+	local k = 1
 	for i = 1, #subs do
 	if subs[i].class == "dialogue" then
-	if index[k] ~= i then vls = vls + 1 values[vls] = i
-	else k = k + 1 end
+	if index[k] ~= i then
+	vls = vls + 1
+	values[vls] = i
+	else
+	k = k + 1
+	end
 	end
 	end
 	return values
