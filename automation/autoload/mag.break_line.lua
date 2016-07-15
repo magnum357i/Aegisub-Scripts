0f2932e0c1f2740f7897c1e1fea75dbc58cb8eea
Berkan KIRAN <berkankiran@hotmail.com>
2016-07-15 12:16:47 +0300

    1.2.1

--- automation/autoload/mag.break_line.lua
+++ automation/autoload/mag.break_line.lua
@@ -1,15 +1,15 @@
 	script_name        = "Break Line"
-	script_description = "Satırlara bölme karakteri ekler."
+	script_description = "Satırlara bölme karakteri ekleyebilir, bunları silebilir veya satırın karakter sayısını kontrol edebilir."
 	script_author      = "Magnum357"
-	script_version     = "1.2"
+	script_version     = "1.2.1"
 
 	mag_import, mag = pcall(require,"mag")
 
 	function remove_line_breaker(subs,sel,config)
-	local style_name   = mag.unstyles(config.u_style_name)
-	local max_char     = config.u_max_char
-	local c            = 0
-	local pcs          = false
+	local style_name = mag.unstyles(config.u_style_name)
+	local max_char   = config.u_max_char
+	local c          = 0
+	local pcs        = false
 	local total_line
 	if style_name == "Tüm stiller" then
 		if config.u_comment_lines then
@@ -34,10 +34,10 @@
 	if not config.u_comment_lines then comment1 = 1 comment2 = 1 end
 	if style_name == "Tüm stiller" then style1 = 1 style2 = 1 end
 		if subs[i].class == "dialogue" and comment1 == comment2 and style1 == style2 then
+			c = c + 1
+			mag.progress("Satır bölmeleri aranıyor...",c,total_line,true,300)
 			if mag.match(line.text,"\\N") then
 			pcs = true
-			c   = c + 1
-			mag.progress("Satır bölmeleri kaldırılıyor...",c,total_line,true,300)
 			line.text = remove_breaker(line.text)
 			end
 			if pcs then
@@ -49,12 +49,12 @@
 	end
 
 	function line_breaker_check(subs,sel,config)
-	local style_name   = mag.unstyles(config.u_style_name)
-	local max_char     = config.u_max_char
-	local msg1         = "Beni böl!"
-	local msg2         = "Beni düzgün böl!"
-	local c            = 0
-	local pcs          = false
+	local style_name = mag.unstyles(config.u_style_name)
+	local max_char   = config.u_max_char
+	local msg1       = "Beni böl!"
+	local msg2       = "Beni düzgün böl!"
+	local c          = 0
+	local pcs        = false
 	local total_line
 	if style_name == "Tüm stiller" then
 		if config.u_comment_lines then
@@ -116,9 +116,9 @@
 	end
 
 	function line_breaker(subs,sel,config)
-	local pcs          = false
-	local style_name   = mag.unstyles(config.u_style_name)
-	local max_char     = config.u_max_char
+	local pcs        = false
+	local style_name = mag.unstyles(config.u_style_name)
+	local max_char   = config.u_max_char
 	for i = 1, #subs do
 	local line = subs[i]
 		if line.class == "dialogue" then
@@ -286,11 +286,11 @@
 
 	function remove_line_breaker_gui(subs)
 	local gui =
-	{{class = "label",                                                                          x = 0, y = 0, width = 1, height = 1, label = "Karakter sınırı:"}
-	,{class = "intedit",  name = "u_max_char",      value = c_max_char,     min = 35, max = 50, x = 1, y = 0, width = 1, height = 1}
-	,{class = "label",                                                                          x = 0, y = 1, width = 1, height = 1, label = mag.wall(" ",18).."Stil:"}
-	,{class = "dropdown", name = "u_style_name",    value = c_style_name,                       x = 1, y = 1, width = 1, height = 1, items = {"Tüm stiller"}, hint = "Sadece kullanılan stiller listelenir. İlk sayı yorum satırı yapılmamış iken ikinci sayı yapılmış satırların sayısıdır."}
-	,{class = "checkbox", name = "u_comment_lines", value = c_comment_line,                     x = 1, y = 2, width = 1, height = 1, label = "Yorum satırlarını geç."}
+	{{class = "label",                                                                         x = 0, y = 0, width = 1, height = 1, label = "Karakter sınırı:"}
+	,{class = "intedit",  name = "u_max_char",     value = c_max_char,     min = 35, max = 50, x = 1, y = 0, width = 1, height = 1}
+	,{class = "label",                                                                         x = 0, y = 1, width = 1, height = 1, label = mag.wall(" ",18).."Stil:"}
+	,{class = "dropdown", name = "u_style_name",   value = c_style_name,                       x = 1, y = 1, width = 1, height = 1, items = {"Tüm stiller"}, hint = "Sadece kullanılan stiller listelenir. İlk sayı yorum satırı yapılmamış iken ikinci sayı yapılmış satırların sayısıdır."}
+	,{class = "checkbox", name = "u_comment_line", value = c_comment_line,                     x = 1, y = 2, width = 1, height = 1, label = "Yorum satırlarını geç."}
 	}
 	mag.styles_insert(subs,gui,4,"comment","")
 	local ok, config = mag.dlg(gui,{"Kaldır","Kapat"})
@@ -306,11 +306,11 @@
 
 	function line_breaker_check_gui(subs)
 	local gui =
-	{{class = "label",                                                                          x = 0, y = 0, width = 1, height = 1, label = "Karakter sınırı:"}
-	,{class = "intedit",  name = "u_max_char",      value = c_max_char,     min = 35, max = 50, x = 1, y = 0, width = 1, height = 1}
-	,{class = "label",                                                                          x = 0, y = 1, width = 1, height = 1, label = mag.wall(" ",18).."Stil:"}
-	,{class = "dropdown", name = "u_style_name",    value = c_style_name,                       x = 1, y = 1, width = 1, height = 1, items = {"Tüm stiller"}, hint = "Sadece kullanılan stiller listelenir. İlk sayı yorum satırı yapılmamış iken ikinci sayı yapılmış satırların sayısıdır."}
-	,{class = "checkbox", name = "u_comment_lines", value = c_comment_line,                     x = 1, y = 2, width = 1, height = 1, label = "Yorum satırlarını geç."}
+	{{class = "label",                                                                         x = 0, y = 0, width = 1, height = 1, label = "Karakter sınırı:"}
+	,{class = "intedit",  name = "u_max_char",     value = c_max_char,     min = 35, max = 50, x = 1, y = 0, width = 1, height = 1}
+	,{class = "label",                                                                         x = 0, y = 1, width = 1, height = 1, label = mag.wall(" ",18).."Stil:"}
+	,{class = "dropdown", name = "u_style_name",   value = c_style_name,                       x = 1, y = 1, width = 1, height = 1, items = {"Tüm stiller"}, hint = "Sadece kullanılan stiller listelenir. İlk sayı yorum satırı yapılmamış iken ikinci sayı yapılmış satırların sayısıdır."}
+	,{class = "checkbox", name = "u_comment_line", value = c_comment_line,                     x = 1, y = 2, width = 1, height = 1, label = "Yorum satırlarını geç."}
 	}
 	mag.styles_insert(subs,gui,4,"comment","")
 	local ok, config = mag.dlg(gui,{"Kontrol et","Kapat"})
