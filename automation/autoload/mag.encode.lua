28f8a6fdf924778ebef566c263c7463079a67252
bademli <berkankiran@hotmail.com>
2025-08-19 00:33:37 +0300

    new: gif maker
    Please reset your config file in appdata.

--- automation/autoload/mag.encode.lua
+++ automation/autoload/mag.encode.lua
@@ -22,32 +22,52 @@
 	in_lang["module_yes"]          = "Git"
 	in_lang["module_no"]           = "Daha Sonra"
 	in_lang["s_name"]              = langs[1].script_name
-	in_lang["s_desc"]              = "FFmpeg kullanarak Klip almanızı sağlar."
+	in_lang["s_desc"]              = "FFmpeg kullanarak klip almanızı veya gif oluşturmanızı sağlar."
 	in_lang["tabKey1"]             = "Pencere"
 	in_lang["guiLabelKey1"]        = "Özel FFmpeg komutları:"
 	in_lang["guiLabelKey2"]        = "Ses olsun"
 	in_lang["guiLabelKey3"]        = "Alt yazı olsun"
 	in_lang["guiLabelKey4"]        = "Çıktı yolu:"
+	in_lang["guiLabelKey5"]        = "FPS:"
+	in_lang["guiLabelKey6"]        = "Genişlik:"
+	in_lang["guiLabelKey7"]        = "[GELİŞMİŞ AYARLAR]"
+	in_lang["guiLabelKey8"]        = "Filtreler:"
+	in_lang["guiLabelKey9"]        = "Palettegen:"
+	in_lang["guiLabelKey10"]       = "Paletteuse:"
 	in_lang["buttonKey1"]          = "Klip"
 	in_lang["buttonKey2"]          = "Bütün video"
+	in_lang["buttonKey3"]          = "GIF"
 	in_lang["buttonKey4"]          = "İptal"
-	in_lang["key1"]                = "FFmpeg Sonucu:\n{%s} kare, {%s} FPS, {%s}"
+	in_lang["buttonKey5"]          = "Yap"
+	in_lang["buttonKey6"]          = "Geri"
+	in_lang["key1"]                = "Video Oluşturuldu: {%s} kare, {%s} FPS, {%s}"
+	in_lang["key2"]                = "GIF Oluşturuldu"
 	elseif lang == langs[2].lang_key then
 	in_lang["module_incompatible"] = "The installed version of the Mag module is incompatible with this lua file!\n\nAt least \"%s\" version or higher of the module file is required.\n\n\nWould you like to go to the download page now?"
 	in_lang["module_not_found"]    = "The module named Mag could not be found!\n\nTo use this file, you need to download the module named mag\nand move it to \"Aegisub/automation/include/\" directory when Aegisub is off.\n\n\nDo you want to go to download page now?"
 	in_lang["module_yes"]          = "Go"
 	in_lang["module_no"]           = "Later"
 	in_lang["s_name"]              = langs[2].script_name
-	in_lang["s_desc"]              = "Allows you to take clips using FFmpeg."
+	in_lang["s_desc"]              = "Allows you to take clips or create gif using FFmpeg."
 	in_lang["tabKey1"]             = "Window"
 	in_lang["guiLabelKey1"]        = "Custom FFmpeg commands:"
 	in_lang["guiLabelKey2"]        = "Put audio"
 	in_lang["guiLabelKey3"]        = "Put subtitle"
 	in_lang["guiLabelKey4"]        = "Output path:"
+	in_lang["guiLabelKey5"]        = "FPS:"
+	in_lang["guiLabelKey6"]        = "Width:"
+	in_lang["guiLabelKey7"]        = "[ADVANCED SETTINGS]"
+	in_lang["guiLabelKey8"]        = "Filters:"
+	in_lang["guiLabelKey9"]        = "Palettegen:"
+	in_lang["guiLabelKey10"]       = "Paletteuse:"
 	in_lang["buttonKey1"]          = "Clip"
 	in_lang["buttonKey2"]          = "Full video"
+	in_lang["buttonKey3"]          = "GIF"
 	in_lang["buttonKey4"]          = "Cancel"
-	in_lang["key1"]                = "FFmpeg Output:\n{%s} frames, {%s} FPS, {%s}"
+	in_lang["buttonKey5"]          = "Make"
+	in_lang["buttonKey6"]          = "Back"
+	in_lang["key1"]                = "Video Created: {%s} frames, {%s} FPS, {%s}"
+	in_lang["key2"]                = "GIF Created"
 	end
 	return in_lang, lang_list, script_name_list
 	end
@@ -60,7 +80,7 @@
 	script_name          = c_lang.s_name
 	script_description   = c_lang.s_desc
 	script_author        = "Magnum357"
-	script_version       = "1.1.0"
+	script_version       = "1.2.0"
 	script_mag_version   = "1.1.5.0"
 	script_file_name     = "mag.encode"
 	script_file_ext      = ".lua"
@@ -73,7 +93,8 @@
 	mag.lang             = c_lang_switch
 
 	c_lock_gui           = false
-	c_buttons1           = {c_lang.buttonKey1, c_lang.buttonKey2, c_lang.buttonKey4}
+	c_buttons1           = {c_lang.buttonKey1, c_lang.buttonKey3, c_lang.buttonKey2, c_lang.buttonKey4}
+	c_buttons2           = {c_lang.buttonKey5, c_lang.buttonKey6, c_lang.buttonKey4}
 	c_cspace_list        = {c_lang.dModeListKey1, c_lang.dModeListKey1, c_lang.dModeListKey2, c_lang.dModeListKey3, c_lang.dModeListKey4}
 	c_lastbutton         = ""
 	c_audiolist          = {}
@@ -82,17 +103,40 @@
 	c.customcommands     = "-c:v libx265 -c:a aac -b:a 128k -x265-params crf=20"
 	c.putaudio           = true
 	c.putsubtitle        = true
-	c.outputpath         = "$subtitle\\encout.mp4"
+	c.putsubtitle2       = false
+	c.outputpath         = "$subtitle\\output{n}.mp4"
+	c.outputpath2        = "$subtitle\\scene{n}.gif"
 	c.selectaudio        = "jpn"
+	c.giffps             = "20"
+	c.gifwidth           = "620"
+	c.giffilter          = "fps=$fps,scale=$width:-1:flags=lanczos"
+	c.palettegenparams   = "stats_mode=diff"
+	c.paletteuseparams   = "dither=bayer:bayer_scale=5:diff_mode=rectangle"
 
-	gui                  = {
-		main1              = {
-		                 {class = "label",                             x = 0, y = 0, width = 35, height = 1, label = c_lang.guiLabelKey4},
+	gui = {
+		main1 = {
+			             {class = "label",                             x = 0, y = 0, width = 35, height = 1, label = c_lang.guiLabelKey4},
 		outputpath     = {class = "edit",     name = "outputpath",     x = 0, y = 1, width = 35, height = 1},
-		                 {class = "label",                             x = 0, y = 2, width = 35, height = 1, label = c_lang.guiLabelKey1},
+		   	             {class = "label",                             x = 0, y = 2, width = 35, height = 1, label = c_lang.guiLabelKey1},
 		customcommands = {class = "textbox",  name = "customcommands", x = 0, y = 3, width = 35, height = 4},
 		putaudio       = {class = "checkbox", name = "putaudio",       x = 0, y = 7, width = 25, height = 1, label=c_lang.guiLabelKey2},
 		putsubtitle    = {class = "checkbox", name = "putsubtitle",    x = 0, y = 8, width = 35, height = 1, label=c_lang.guiLabelKey3}
+		},
+		main2 = {
+		   	               {class = "label",                               x = 0, y = 0,  width = 35, height = 1, label = c_lang.guiLabelKey4},
+		outputpath2      = {class = "edit",     name = "outputpath2",      x = 0, y = 1,  width = 35, height = 1},
+		   	               {class = "label",                               x = 0, y = 2,  width = 5,  height = 1, label = c_lang.guiLabelKey5},
+		giffps           = {class = "intedit",  name = "giffps",           x = 0, y = 3,  width = 5,  height = 1},
+		   	               {class = "label",                               x = 0, y = 4,  width = 5,  height = 1, label = c_lang.guiLabelKey6},
+		gifwidth         = {class = "intedit",  name = "gifwidth",         x = 0, y = 5,  width = 5,  height = 1},
+		putsubtitle2     = {class = "checkbox", name = "putsubtitle2",     x = 0, y = 6,  width = 5,  height = 1, label=c_lang.guiLabelKey3},
+		                   {class = "label",                               x = 0, y = 8,  width = 5,  height = 1, label = c_lang.guiLabelKey7},
+		                   {class = "label",                               x = 0, y = 9,  width = 5,  height = 1, label = c_lang.guiLabelKey8},
+		giffilter        = {class = "edit",     name = "giffilter",        x = 0, y = 10, width = 35, height = 1},
+		                   {class = "label",                               x = 0, y = 11, width = 5,  height = 1, label = c_lang.guiLabelKey9},
+		palettegenparams = {class = "edit",     name = "palettegenparams", x = 0, y = 12, width = 35, height = 1},
+		                   {class = "label",                               x = 0, y = 13, width = 5,  height = 1, label = c_lang.guiLabelKey10},
+		paletteuseparams = {class = "edit",     name = "paletteuseparams", x = 0, y = 14, width = 35, height = 1}
 		}
 	}
 	end
@@ -122,6 +166,8 @@
 	local fcommand = ""
 	--input video
 	fcommand = fcommand..mag.string.format("-i \"{%s}\"", pathconversion(getfilepath("video")))
+	--sync
+	fcommand = fcommand.." -copyts"
 	--trim
 	if mag.n(startt) ~= 0 and mag.n(endt) ~= 0 then
 	fcommand = fcommand.." "..mag.string.format("-ss {%s} -to {%s}", mag.convert.ms_to_time(startt), mag.convert.ms_to_time(endt))
@@ -164,6 +210,14 @@
 	outputpath       = mag.gsub(outputpath, "$subtitle", aegisub.decode_path("?script"))
 	outputpath       = mag.gsub(outputpath, "$video",    aegisub.decode_path("?video"))
 	outputpath       = pathconversion(outputpath)
+	if mag.match(outputpath, "{n}") then
+		for i = 1, 999 do
+			if not file_exists(mag.gsub(outputpath, "{n}", tostring(i))) then
+			outputpath = mag.gsub(outputpath, "{n}", tostring(i))
+			break
+			end
+		end
+	end
 	fcommand         = fcommand.." ".."\""..outputpath.."\""
 	mag.show.log(fcommand)
 	return mag.string.format("ffmpeg {%s} 2>&1", fcommand)
@@ -189,6 +243,81 @@
 	estatistics(output)
 	end
 
+	function ffmpegline2(startt,endt)
+	local fcommand = ""
+	--trim
+	fcommand = fcommand.." "..mag.string.format("-ss {%s} -to {%s}", mag.convert.ms_to_time(startt), mag.convert.ms_to_time(endt))
+	--input video
+	fcommand = fcommand..mag.string.format(" -i \"{%s}\"", pathconversion(getfilepath("video")))
+	--sync
+	fcommand = fcommand.." -copyts"
+	--filter and palette
+	local convertedfilter = c.giffilter
+	convertedfilter = mag.gsub(convertedfilter, "$fps",   c.giffps)
+	convertedfilter = mag.gsub(convertedfilter, "$width", c.gifwidth)
+	fcommand        = fcommand.." "..mag.string.format("-filter_complex \"{%s},split[a][b];[a]palettegen[p];[b][p]paletteuse\"", convertedfilter)
+	--palettegen
+	if not mag.is.empty(c.palettegenparams) then
+	fcommand        = mag.gsub(fcommand, "palettegen", "palettegen="..c.palettegenparams)
+	end
+	--paletteuse
+	if not mag.is.empty(c.paletteuseparams) then
+	fcommand        = mag.gsub(fcommand, "paletteuse", "paletteuse="..c.paletteuseparams)
+	end
+	--subtitle
+	if c.putsubtitle2 then
+	local subpath = pathconversion(getfilepath("subtitle"))
+	subpath = mag.gsub(subpath, ":", "\\:")
+	fcommand = mag.gsub(fcommand, "(%-filter_complex%s+\")", "%1"..mag.string.format("ass='{%s}',", subpath))
+	end
+	--overwrite
+	fcommand = fcommand.." ".."-y"
+	--output video
+	local outputpath = c.outputpath2
+	outputpath       = mag.gsub(outputpath, "$subtitle", aegisub.decode_path("?script"))
+	outputpath       = mag.gsub(outputpath, "$video",    aegisub.decode_path("?video"))
+	outputpath       = pathconversion(outputpath)
+	if mag.match(outputpath, "{n}") then
+		for i = 1, 999 do
+			if not file_exists(mag.gsub(outputpath, "{n}", tostring(i))) then
+			outputpath = mag.gsub(outputpath, "{n}", tostring(i))
+			break
+			end
+		end
+	end
+	fcommand = fcommand.." ".."\""..outputpath.."\""
+	mag.show.log(fcommand)
+	return mag.string.format("ffmpeg {%s} 2>&1", fcommand)
+	end
+
+	function gifvideo(subs,sel)
+	local vstart, vend = 0, 0
+	local lines_index = mag.line.index(subs, sel, mag.window.lang.message("selected_lines"), false, false)
+	for i = 1, #lines_index do
+	index = lines_index[i]
+	line  = subs[index]
+		if line.start_time < vstart or vstart == 0 then
+		vstart = line.start_time
+		end
+		if line.end_time > vend or vend == 0 then
+		vend = line.end_time
+		end
+	end
+	mag.show.log("Processing...")
+	local output = runcommand(ffmpegline2(vstart,vend))
+	estatistics2(output)
+	end
+
+	function file_exists(name)
+	local f = io.open(name, 'r')
+	if f ~= nil then
+	io.close(f)
+	return true
+	else
+    return false
+	end
+	end
+
 	function estatistics(output)
 	local staline = mag.match(output,"\n(encoded [^\n]*)\n")
 	if staline then
@@ -196,6 +325,15 @@
 	local totalframe = mag.match(staline, "encoded ([%d]+) frames")
 	local elevatedtime = mag.match(staline, "frames in ([%d%.]+[sm])")
 	if mag.n(totalframe) > 0 then mag.show.log(mag.string.format(c_lang.key1,totalframe,fps,elevatedtime)) end
+	else
+	mag.show.log(output)
+	end
+	end
+
+	function estatistics2(output)
+	local staline = mag.match(output,"fps=0.0")
+	if staline then
+	mag.show.log(c_lang.key2)
 	else
 	mag.show.log(output)
 	end
@@ -216,17 +354,27 @@
 	end
 
 	function add_macro1(subs, sel)
-	mag.config.put(gui.main1)
 	if #c_audiolist == 0 then c_audiolist = getaudiolist() end
 	if #c_audiolist > 1 then
 	local selectaudio = (mag.array.search(c_audiolist, c.selectaudio)) and c.selectaudio or c_audiolist[1]
 	gui.main1.selectaudio = {class = "dropdown", name = "selectaudio", x = 25, y = 7, width = 10, height = 1, items=c_audiolist, value=selectaudio}
 	end
-	local ok, config = mag.window.dialog(gui.main1, c_buttons1)
+	local ok, config
+	repeat
+		if ok == mag.convert.ascii(c_buttons1[2]) then
+		mag.config.put(gui.main2)
+		ok, config = mag.window.dialog(gui.main2, c_buttons2)
+		else
+		mag.config.put(gui.main1)
+		ok, config = mag.window.dialog(gui.main1, c_buttons1)
+		end
 	mag.config.take(config)
-	if ok == mag.convert.ascii(c_buttons1[1]) or ok == mag.convert.ascii(c_buttons1[2]) then
+	until ok == mag.convert.ascii(c_buttons1[1]) or ok == mag.convert.ascii(c_buttons1[3]) or ok == mag.convert.ascii(c_buttons1[4]) or ok == mag.convert.ascii(c_buttons2[1])
+	if ok == mag.convert.ascii(c_buttons1[1]) or ok == mag.convert.ascii(c_buttons1[3]) then
 	c_lastbutton = ok
 	encodevideo(subs, sel)
+	elseif ok == mag.convert.ascii(c_buttons2[1]) then
+	gifvideo(subs, sel)
 	end
 	end
 
